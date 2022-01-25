daf92180c0e6e623c5196440bb8b1264be993a8e
Shamit Nibras <Nibras98>
2022-01-26 01:04:28 +0600

    Task

---
+++ Mid/CV/CV/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EducationalBackground()
+        {
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            
+
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}

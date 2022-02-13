1c1c02afcd0ec9fdb531f9ab59ce8df496cace42
ShamitNibras <19-40117-1@student.aiub.edu>
2022-02-13 22:30:29 +0600

    LabTask-3

---
+++ Mid/LabTask-3/LabTask-3/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using LabTask_3.Models.Student;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTask_3.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            StudentEntities db = new StudentEntities();
+            var data = db.Students.ToList();
+            return View(data);
+        }
+        [HttpPost]
+
+        public ActionResult Create(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                StudentEntities db = new StudentEntities();
+                db.Students.Add(s);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
